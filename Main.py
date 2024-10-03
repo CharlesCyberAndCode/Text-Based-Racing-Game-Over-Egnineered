91f9e7e41d69b01667a2f6019e201caf0d74c906
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-10-03 19:23:56 -0400

    Edits
    Removed body list and variable as it had to convey 4 variables. I think I will be using a class of "body" and have the variables assigned to it from there. Then have those variables sent to user's car to clean up the code and make a bit simpler. Plans to have a config file for a rough and dirty "save" "load" functionality. Simulations are done and now need to be implemented in the code, as well as build the rest of the code around them.

--- Main.py
+++ Main.py
@@ -8,28 +8,35 @@ engineList = [300,150]
 # Tires as well will have values relating to a named part for user to buy
 tireList = [1.2,1.5]
 # Will figure out bodies. Bodies need 3 values at the moment, so may simplify to 3 lists ->
-# A bodiesCD list for coefficient of drag, a bodiesFA for frontal are, ->
+# A bodiesCD list for coefficient of drag, a bodiesFA for frontal area, ->
 # and a bodiesCOG for center of gravity. Currently it shows CD
-bodyList = [.5,.7]
-
+### MAY STILL USE THIS WILL PROBABLY USE THE CLASS AND OBJECTS INSTEAD
 
 # Main car class that all others will be from
+# Will implement body variables in future
 class Car:
-    def __init__(self, engine, body, tire):
+    def __init__(self, engine, tire):
         self.engine = engine
-        self.body = body
         self.tire = tire
+        pass
+## Body variable has too many things to convey, will take variables from this class for user car
+class Body:
+    def __init__(self,coefDrag,cog,weight,frontal):
+        self.coefDrag = coefDrag
+        self.cog = cog
+        self.weight = weight
+        self.frontal = frontal
         pass
 
 
+
 # Users car. This will change as the game goes on.     
-userCar = Car(engineList[0], bodyList[0], tireList[0])
+userCar = Car(engineList[0], tireList[0])
 print(userCar.engine)
-print(userCar.body)
 print(userCar.tire)
 
 #First AI car that will have a set part collection.
-johnCar = Car(engineList[1], bodyList[1], tireList[1])
+johnCar = Car(engineList[1], tireList[1])
 ## johnCar.engine = engineList[0]
 # simple way to change object values based on user input
 
