d53265f36ec6113c5c83adeed931afedf4ce9daf
CharlesCodes <136041610+CharlesCyberAndCode@users.noreply.github.com>
2024-10-03 23:43:55 -0400

    New
    Inputted some values for different bodies. Changed tire grip coefficient values to more accurately reflect the time.

--- Main.py
+++ Main.py
@@ -1,12 +1,15 @@
 ### Text Based Racing Game Main
 import math
 
-
+'''
+VARIABLES AND CLASS INITIALIZATION
+'''
 # Will have these filled with different values to represent the car user has chosen.
-# Engine will be the horsepower they have for now Will create list to have names and values organized
-engineList = [300,150]
+# Engine will be the horsepower they have for now Will create spreadsheet to have ->
+# names and values organized for all variables
+engineList = [300,150,50]
 # Tires as well will have values relating to a named part for user to buy
-tireList = [1.2,1.5]
+tireList = [.4,.5,.6,.8,.9,1,1.2]
 # Will figure out bodies. Bodies need 3 values at the moment, so may simplify to 3 lists ->
 # A bodiesCD list for coefficient of drag, a bodiesFA for frontal area, ->
 # and a bodiesCOG for center of gravity. Currently it shows CD
@@ -15,9 +18,13 @@ tireList = [1.2,1.5]
 # Main car class that all others will be from
 # Will implement body variables in future
 class Car:
-    def __init__(self, engine, tire):
+    def __init__(self, engine, tire,cog,weight,frontal,coefDrag,):
         self.engine = engine
         self.tire = tire
+        self.cog = cog
+        self.weight = weight
+        self.frontal = frontal
+        self.coefDrag = coefDrag
         pass
 ## Body variable has too many things to convey, will take variables from this class for user car
 class Body:
@@ -27,7 +34,12 @@ class Body:
         self.weight = weight
         self.frontal = frontal
         pass
-
+suv = Body(.45,.5,1750,2.75)
+van = Body(.6,1,2200,3.5)
+sport = Body(.3,.3,1350,2)
+muscle = Body(.35,.3,1450,2.2)
+miura = Body(.25,.25,1050,1.8)
+beetle = Body(.4,.45,850,1.6)
 
 
 # Users car. This will change as the game goes on.     
@@ -40,3 +52,11 @@ johnCar = Car(engineList[1], tireList[1])
 ## johnCar.engine = engineList[0]
 # simple way to change object values based on user input
 
+'''
+CODE FOR AI CARS
+'''
+
+
+'''
+MAIN LOOP
+'''
